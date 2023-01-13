b6232e639353f5b6a18a7d04a5a957104c3f1d0e
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-01-13 10:52:46 -0300

    get and post participants without validation

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 app.use(cors());
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
@@ -18,5 +19,34 @@ catch(err) {
 }
 
 const db = mongoClient.db();
+
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+    try
+    {
+        const users = await db.collection("participants").findOne({ name });
+        if (users) return res.status(409).send("usuário já cadastrado");
+        await db.collection("participants").insertOne({
+            name: name,
+            lastStatus: Date.now()
+        });
+        res.sendStatus(201);
+    }
+    catch (err)
+    {
+        res.status(500).send(err);
+    }
+});
+
+app.get("/participants", async (_, res) => {
+    try {
+        res.send(await db.collection("participants").find().toArray());
+    } catch (err) {
+        return res.send(err);
+    }
+})
+
 
 app.listen(5000, () => console.log("servidor conectado"));
