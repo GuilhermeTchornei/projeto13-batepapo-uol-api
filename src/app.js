8a353b72ae205e2d59aa9384cf9b01956bd31e34
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-01-14 18:04:27 -0300

    message and participant validation

--- src/app.js
+++ src/app.js
@@ -28,6 +28,11 @@ app.post("/participants", async (req, res) => {
     try
     {
         const { name } = req.body;
+
+        const nameSchema = joi.object({ name: joi.string().required() });
+        const validation = nameSchema.validate({name});
+        if (validation.error) return res.status(422).send(validation.error);
+
         const users = await db.collection("participants").findOne({ name });
         if (users) return res.status(409).send("usuário já cadastrado");
 
@@ -66,9 +71,19 @@ app.post("/messages", async (req, res) => {
     {
         const { to, text, type } = req.body;
         const from = req.headers.user;
-        const message = { to, text, type, from, time: dayjs().format("hh:mm:ss") };
-        console.log(message);
-        await db.collection("messages").insertOne(message);
+
+        if (!await db.collection("participants").findOne({ name: from })) return res.sendStatus(422);
+        const messageSchema = joi.object({
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.string().valid('message', 'private_message').required(),
+            from: joi.string().required()
+        });
+
+        const message = { to, text, type, from };
+        const validation = messageSchema.validate(message, { abortEarly: false });
+        if (validation.error) return res.status(422).send(validation.error);
+        await db.collection("messages").insertOne({...message, time: dayjs().format("hh:mm:ss")});
         res.sendStatus(201);
     } catch (err)
     {
@@ -84,7 +99,7 @@ app.get("/messages", async (req, res) => {
 
         const messages = await db.collection("messages").find({
             $or: [
-                { type: 'public' },
+                { type: 'message' },
                 { type: 'status' },
                 { type: "private_message", from },
                 { type: "private_message", to: from }
