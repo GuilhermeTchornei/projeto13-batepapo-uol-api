d84db93ae6237da709253dec519efe35054b812e
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-01-14 14:33:14 -0300

    post and get messages without validation

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import dayjs from 'dayjs';
+import joi from 'joi';
 
 dotenv.config();
 const app = express();
@@ -22,10 +24,9 @@ const db = mongoClient.db();
 
 
 app.post("/participants", async (req, res) => {
-    const { name } = req.body;
-    console.log(name);
     try
     {
+        const { name } = req.body;
         const users = await db.collection("participants").findOne({ name });
         if (users) return res.status(409).send("usuário já cadastrado");
         await db.collection("participants").insertOne({
@@ -41,10 +42,46 @@ app.post("/participants", async (req, res) => {
 });
 
 app.get("/participants", async (_, res) => {
-    try {
+    try
+    {
         res.send(await db.collection("participants").find().toArray());
+    } catch (err)
+    {
+        return res.send(err.message);
+    }
+});
+
+app.post("/messages", async (req, res) => {
+    try
+    {
+        const { to, text, type } = req.body;
+        const from = req.headers.user;
+        const message = { to, text, type, from, time: dayjs().format("hh:mm:ss") };
+        console.log(message);
+        await db.collection("messages").insertOne(message);
+        res.sendStatus(201);
+    } catch (err)
+    {
+        return res.send(err.message);
+    }
+});
+
+app.get("/messages", async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit) || 0;
+        const from = req.headers.user;
+
+        const messages = await db.collection("messages").find({
+            $or: [
+                { type: 'public' },
+                { type: "private_message", from },
+                { type: "private_message", to: from }
+            ]
+        }).toArray();
+
+        res.send(messages.slice(-limit));
     } catch (err) {
-        return res.send(err);
+        res.send(err);
     }
 })
 
